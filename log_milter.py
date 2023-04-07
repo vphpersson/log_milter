a7db05f5b3397fe2d0630f6bffbec2bcc411f585
vph <vph.persson@gmail.com>
2023-04-08 00:22:15 +0200

    Remove decorator.

--- log_milter.py
+++ log_milter.py
@@ -114,7 +114,6 @@ class LogMilter(MilterBase):
 
         return MILTER_CONTINUE
 
-    @milter_noreply
     def header_bytes(self, fld: str, val: bytes):
         self._message.write(fld.encode(encoding='ascii') + b': ' + val + b'\n')
 
